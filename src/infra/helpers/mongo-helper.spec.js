876bfcd47dc5592529f5f458a10b0f5ef0099f73
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2021-03-01 23:45:46 -0300

    ensure UpdateAccessTokenRepository updates user with the given accessToken

--- src/infra/helpers/mongo-helper.spec.js
+++ src/infra/helpers/mongo-helper.spec.js
@@ -1,6 +1,10 @@
 const MongoHelper = require('./mongo-helper')
 
 describe('Mongo Helper', () => {
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
   test('Should reconnect when getDb() is invoked and client is disconnected', async () => {
     const sut = MongoHelper
     await sut.connect(process.env.MONGO_URL)
